03bd53a0807c771a1a166186c39ae61fc577e614
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2024-12-09 13:40:39 +0700

    feat: add state to change tab

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,8 @@ import logo from "./assets/logo.png";
 import NavItems from "./components/NavItems";
 
 function App() {
+  const [activeTab, setActiveTab] = useState("Dashboard");
+
   const menu = [
     {
       title: "Dashboard",
@@ -25,12 +27,21 @@ function App() {
     },
   ];
 
+  const handleClick = (activeTab) => {
+    setActiveTab(activeTab);
+  };
+
   return (
     <>
+      {/* <nav className="fixed top-0 left-0 flex items-center w-screen justify-between bg-white py-4 px-8 z-50 shadow-md"> */}
       <nav className="flex items-center w-screen justify-between bg-white py-4 px-8">
         <img src={logo} alt="" />
         <div className="flex gap-x-8">
-          <NavItems menu={menu} />
+          <NavItems
+            menu={menu}
+            activeTab={activeTab}
+            handleClick={setActiveTab}
+          />
         </div>
       </nav>
     </>
--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -1,8 +1,19 @@
-function NavItems({ menu }) {
+import { useState } from "react";
+
+function NavItems({ menu, activeTab, handleClick }) {
   return (
     <div className="flex gap-x-8">
       {menu.map((item, index) => (
-        <a key={index} href={item.link}>
+        <a
+          onClick={() => {
+            handleClick(item.title);
+          }}
+          className={
+            activeTab !== item.title ? "text-black" : "text[#19918F] font bold"
+          }
+          key={index}
+          href={item.link}
+        >
           {item.title}
         </a>
       ))}
