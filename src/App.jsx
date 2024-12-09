44e131390748f9b622b8adc5f5f17530e9892fe2
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2024-12-09 11:46:13 +0700

    feat: add new nav item component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,37 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import logo from "./assets/logo.png";
+import NavItems from "./components/NavItems";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const menu = [
+    {
+      title: "Dashboard",
+      link: "#",
+    },
+    {
+      title: "Transfer",
+      link: "#",
+    },
+    {
+      title: "Topup",
+      link: "#",
+    },
+    {
+      title: "Sign Out",
+      link: "#",
+    },
+  ];
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="text-3xl font-bold underline">Walled</p>
+      <nav className="flex items-center w-screen justify-between bg-white py-4 px-8">
+        <img src={logo} alt="" />
+        <div className="flex gap-x-8">
+          <NavItems menu={menu} />
+        </div>
+      </nav>
     </>
   );
 }
