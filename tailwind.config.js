97d36b258e22bc387c235154288fbac55db10ad0
Rahmat Faris Akbar <rfarisakbar@gmail.com>
2024-12-09 11:02:59 +0700

    chore: add tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   // content: [],
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
